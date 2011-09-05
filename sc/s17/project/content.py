5e06e956d1f0150ead9c065431ca66a32461862d
hvelarde <hector.velarde@gmail.com>
2011-09-05 14:27:24 -0300

    added default view; still has registration issues

--- sc/s17/project/content.py
+++ sc/s17/project/content.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from five import grok
-from zope import schema
 
 from plone.directives import form, dexterity
 
-from sc.s17.project import MessageFactory as _
-
 
 class IProject(form.Schema):
-
     pass
+
+
+class View(grok.View):
+    grok.context(IProject)
+    grok.require('zope2.View')
